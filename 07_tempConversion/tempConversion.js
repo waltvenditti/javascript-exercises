872230d7ee645e5cd9b54ae549a145a084205446
WaltVenditi <waltvenditti@gmail.com>
2021-11-01 16:11:17 -0400

    finished ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(tempInF) {
+  let tempInC; 
+  tempInC = (tempInF - 32) * (5/9); 
+  tempInC = roundToOneDecimal(tempInC);
+  return tempInC
 };
 
-const ctof = function() {
-
+const ctof = function(tempInC) {
+  let tempInF;
+  tempInF = tempInC * (9/5) + 32;
+  tempInF = roundToOneDecimal(tempInF);
+  return tempInF;
 };
+
+function roundToOneDecimal(float) {
+  float *= 10;
+  float = Math.round(float);
+  float /= 10;
+  return float; 
+}
 
 // Do not edit below this line
 module.exports = {
